9fe6b1d4c0b7555d5290b3abbacf4334275fd205
Pradeep kumar parida <83020609+pradeep-1995@users.noreply.github.com>
2023-01-20 13:46:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie
 import pandas as pd
 import preprocessor,helper
 import matplotlib.pyplot as plt
